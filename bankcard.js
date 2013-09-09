96800aef8b0bacc8a6129822e5432f66720c66b1
hulufei <f.calabash@gmail.com>
2013-09-09 16:25:08 +0800

    implemented

---
+++ bankcard.js
@@ -0,0 +1,60 @@
+/**
+ * Bank card input control plugin, split card number into 4 groups, every
+ * group include 4 numbers. And a pop layer will show up to emphasize the
+ * input value.
+ *
+ */
+~function($) {
+  $.fn.bankcard = function(options) {
+    options = options || {};
+
+    var $input = this
+      , $pop
+      , popClass = options.pop || '';
+
+    $input.on('keypress', function(e) {
+      var s = String.fromCharCode(e.which)
+        , value = $input.val()
+        , position;
+
+      if (!/[\d\b]/.test(s) || /^(\d{4}\s){3}\d{4}$/.test(value)) return false;
+
+      // Else input number
+      if (!$pop) {
+        position = $input.position();
+
+        $pop = $('<div/>').css({
+          position: 'relative',
+          left: position.left,
+          top: position.top - $input.height()
+        });
+
+        if (popClass) $pop.addClass(popClass);
+
+        $input.after($pop);
+      }
+    });
+
+    // Actually got the input value in keyup
+    $input.on('keyup', function(e) {
+      var value = $input.val()
+        , i , arr;
+
+      if (value === '' && $pop) {
+        $pop.hide();
+      }
+      else if (e.which !== 8 && value.length >= 4) {
+        // Split into groups
+        arr = value.split(' ');
+        for (i = 0; i < arr.length; i++) {
+          // Except last group
+          if (i < 3 && arr[i].length === 4) arr[i] += ' ';
+        }
+        value = arr.join('');
+        $input.val(value);
+      }
+
+      if ($pop) $pop.html(value);
+    });
+  };
+}(jQuery);
---
+++ test/spec/test.js
@@ -0,0 +1,86 @@
+/*global describe, it */
+'use strict';
+
+describe('Bankcard Input Plugin', function () {
+  beforeEach(function() {
+    this.$fixture = $('<div/>').appendTo('body');
+    this.$input = $('<input type="text"/>').appendTo(this.$fixture);
+  });
+
+  afterEach(function() {
+    this.$fixture.remove();
+  });
+
+  it('should work as a jQuery plugin', function() {
+    expect(this.$input.bankcard).to.exist;
+  });
+
+  it('should ignore the input that is not a number', function() {
+    this.$input.bankcard();
+    this.$input.autotype('a');
+
+    expect(this.$input.val()).to.equal('');
+    expect(this.$input.siblings('div')).to.have.length(0);
+  });
+
+  it('should input numbers', function() {
+    this.$input.bankcard();
+    this.$input.autotype('18');
+    expect(this.$input.val()).to.equal('18');
+  });
+
+  it('should enable back-space', function() {
+    this.$input.bankcard();
+    this.$input.autotype('186{{back}}');
+    expect(this.$input.val()).to.equal('18');
+  });
+
+  it('should pop over a layer emphasize the input number', function() {
+    this.$input.bankcard();
+    this.$input.autotype('1');
+
+    expect(this.$input.val()).to.equal('1');
+    expect(this.$input.siblings('div').length).to.be.equal(1);
+    expect(this.$input.siblings('div').is(':hidden')).to.be.false;
+    expect(this.$input.siblings('div').text()).to.be.equal('1');
+  });
+
+  it('can specify class to pop over layer', function() {
+    this.$input.bankcard({ pop: 'customStyle' });
+    this.$input.autotype('1');
+
+    expect(this.$input.siblings('div').hasClass('customStyle')).to.be.true;
+  });
+
+  it('should split input numbers into groups every four', function() {
+    this.$input.bankcard();
+    this.$input.autotype('12345');
+
+    expect(this.$input.val()).to.equal('1234 5');
+    expect(this.$input.siblings('div').length).to.be.equal(1);
+    expect(this.$input.siblings('div').text()).to.be.equal('1234 5');
+  });
+
+  it('should delete back normally in groups', function() {
+    this.$input.bankcard();
+    this.$input.autotype('1234{{back}}');
+
+    expect(this.$input.val()).to.equal('1234');
+    expect(this.$input.siblings('div').text()).to.be.equal('1234');
+  });
+
+  it('should limit to 16 numbers', function() {
+    this.$input.bankcard();
+    this.$input.autotype('8888 8888 8888 8888');
+
+    expect(this.$input.val()).to.equal('8888 8888 8888 8888');
+    expect(this.$input.siblings('div').text()).to.be.equal('8888 8888 8888 8888');
+  });
+
+  it('should hide the emphasize layer when input empty', function() {
+    this.$input.bankcard();
+    this.$input.autotype('1{{back}}');
+
+    expect(this.$input.siblings('div').is(':hidden')).to.be.true;
+  });
+});
